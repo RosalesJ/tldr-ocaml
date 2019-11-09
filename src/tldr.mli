6490de79a3d8897e6664b3fb09f83f7f68feb8e6
Jacob Rosales Chase <coby@case.edu>
2019-11-08 20:58:28 -0500

    Working but grey

--- src/tldr.mli
+++ src/tldr.mli
@@ -1,7 +1,1 @@
-open Core
-
-val no_documentation : String.t -> String.t
-
-val display : String.t -> unit
-
-val display_page : String.t -> String.t -> unit
+val display_page : String.t -> String.t -> unit Async.Deferred.t
