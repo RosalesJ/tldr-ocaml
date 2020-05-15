8992b5c164684205e3cbbcfdc963667607e86dfb
Jacob Rosales Chase <coby@case.edu>
2020-05-14 21:40:23 -0400

    Basics of cmdliner

--- src/tldr.ml
+++ src/tldr.ml
@@ -1,44 +1,52 @@
 open Base
 open Common
+open Cmdliner
 
+let (<*>) = Term.($)
 
 let no_documentation =
-  Printf.sprintf "`%s` documentation is not available. \n Consider contributing Pull Request to https://github.com/tldr-pages/tldr\n"
+  Printf.sprintf "`%s` documentation is not available. \n Consider contributing Pull Request to https://github.com/tldr-pages/tldr"
 
 let usage_message = Printf.sprintf "%s - %s\n\n%s"
     "tldr"
     "Simplified man pages"
     "Usage: tldr <command>"
 
-let doc = Printf.sprintf "%s\n"
+let _doc = Printf.sprintf "%s"
     usage_message
 
-let display_page command platform =
+let display_page command platform update_cache =
+  ignore update_cache;
   match get_page command platform with
-  | Missing -> Stdio.printf "%s" (no_documentation command)
-  | Error e -> Stdio.printf "%s" e
+  | Missing -> Stdio.printf "%s\n" (no_documentation command)
+  | Error e -> Stdio.printf "%s\n" e
   | Success page -> Display.display page
 
-let () =
-  let os = ref Environment.system in
-  let update = ref false in
-  let command = ref "" in
+let command =
+  let doc = "Display the tldr page for $(docv)" in
+  Arg.(value & pos_all string [] & info [] ~docv:"command" ~doc)
 
-  let append_to_command str =
-    if String.equal !command "" then
-      command := str
-    else
-      command := String.concat ~sep:"-" [!command; str]
+let platform =
+  let doc = "Display the command for a given $(docv)" in
+  Arg.(value & opt string Environment.system & info ["p"; "platform"] ~docv:"PLATFORM" ~doc)
+
+let update_cache =
+  let doc = "Update the local cache" in
+  Arg.(value & flag & info ["u"; "update"] ~docv: "update" ~doc)
+
+let () =
+  let info =
+    let doc = "Simplified man pages" in
+    let man = [
+        `S Manpage.s_bugs;
+        `P "Email bug reports to <coby@case.edu>." ]
+    in
+    Term.info "tldr" ~version:"0.3" ~doc ~exits:Term.default_exits ~man
   in
 
-  let spec_list = [("--os", Caml.Arg.Set_string os, "Override the operating System [linux, osx, sunos, windows]");
-                   ("-p", Caml.Arg.Set_string os, "Override the operating System [linux, osx, sunos, windows]");
-                   ("--update", Caml.Arg.Set update, "Update the cached commands");
-                   ("--", Caml.Arg.Rest append_to_command, "The actual command")] in
+  let run_t = Term.(const display_page
+                    <*> (pure (String.concat ~sep:"-") <*> command)
+                    <*> platform
+                    <*> update_cache) in
 
-  Caml.Arg.parse spec_list append_to_command doc;
-
-  if String.equal !command "" then
-    Stdio.printf "%s\n" usage_message
-  else
-    display_page !command !os
+  Term.exit @@ Term.eval (run_t, info)
