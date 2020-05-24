49ddfe86bb9f215fed01cb380de38650203ecd56
Jacob Rosales Chase <coby@case.edu>
2020-05-23 21:59:04 -0400

    Generate and install docs with dune

--- src/tldr.ml
+++ src/tldr.ml
@@ -2,18 +2,8 @@ open Base
 open Common
 open Cmdliner
 
-let (<*>) = Term.($)
-
 let no_documentation =
   Printf.sprintf "`%s` documentation is not available. \n Consider contributing Pull Request to https://github.com/tldr-pages/tldr"
-
-let usage_message = Printf.sprintf "%s - %s\n\n%s"
-    "tldr"
-    "Simplified man pages"
-    "Usage: tldr <command>"
-
-let _doc = Printf.sprintf "%s"
-    usage_message
 
 let display_page command platform update_cache =
   ignore update_cache;
@@ -22,31 +12,44 @@ let display_page command platform update_cache =
   | Error e -> Stdio.printf "%s\n" e
   | Success page -> Display.display page
 
-let command =
-  let doc = "Display the tldr page for $(docv)" in
-  Arg.(value & pos_all string [] & info [] ~docv:"command" ~doc)
+module Args = struct
+  open Arg
 
-let platform =
-  let doc = "Display the command for a given $(docv)" in
-  Arg.(value & opt string Environment.system & info ["p"; "platform"] ~docv:"PLATFORM" ~doc)
+  let command =
+    let doc = "Display the tldr page for $(docv)" in
+    non_empty
+    & pos_all string []
+    & info [] ~docv:"COMMAND" ~doc
 
-let update_cache =
-  let doc = "Update the local cache" in
-  Arg.(value & flag & info ["u"; "update"] ~docv: "update" ~doc)
+  let platform =
+    let doc = "Display the command for a given $(docv). Options can be one of [linux, sunos, osx]." in
+    value
+    & opt string Environment.system
+    & info ["p"; "platform"] ~docv:"PLATFORM" ~doc
+
+  let update_cache =
+    let doc = "Update the local cache." in
+    value
+    & flag
+    & info ["u"; "update"] ~docv:"UPDATE" ~doc
+end
 
 let () =
+  let open Term in
+
   let info =
     let doc = "Simplified man pages" in
     let man = [
-        `S Manpage.s_bugs;
-        `P "Email bug reports to <coby@case.edu>." ]
+      `S Manpage.s_bugs;
+      `P "Email bug reports to <coby@case.edu>." ]
     in
-    Term.info "tldr" ~version:"0.3" ~doc ~exits:Term.default_exits ~man
+    info "tldr" ~version:"0.2" ~doc ~man
   in
 
-  let run_t = Term.(const display_page
-                    <*> (pure (String.concat ~sep:"-") <*> command)
-                    <*> platform
-                    <*> update_cache) in
+  let run_t = const display_page
+              $ (const (String.concat ~sep:"-") $ Args.command)
+              $ Args.platform
+              $ Args.update_cache
+  in
 
-  Term.exit @@ Term.eval (run_t, info)
+  exit @@ eval (run_t, info)
